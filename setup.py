9915967e5053c6281cfce1eff75cafc0ba77066e
kianz20 <kianja02@gmail.com>
2024-09-24 00:24:29 +1200

    Add setup.py for python library setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='carjam',
+    version='1.3.3',
+    description='Unoffical Python API for CarJam',
+    url='https://github.com/kianz20/CarJam-API',
+    author='Kian Jazayeri',
+    author_email='kianja02@gmail.com',
+    license='unlicense',
+    packages=find_packages(),
+    zip_safe=False
+)
