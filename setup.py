0c88e1758d2b5328485a07dfea2eada39e225682
kianz20 <kianja02@gmail.com>
2024-09-25 00:20:45 +1200

    Add License file and edit setup.py in preparation for publish

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='carjam',
-    version='1.3.3',
-    description='Unoffical Python API for CarJam',
+    version='1.0.0',
+    description='Unoffical Python API for CarJam NZ',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     url='https://github.com/kianz20/CarJam-API',
     author='Kian Jazayeri',
     author_email='kianja02@gmail.com',
-    license='unlicense',
     packages=find_packages(),
-    zip_safe=False
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License", 
+        "Operating System :: OS Independent",
+    ],
 )
