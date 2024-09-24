6e73370f5cfc7c522c9eab05bd6ad456ef7262a6
kianz20 <kianja02@gmail.com>
2024-09-25 01:10:18 +1200

    Update setup to new post version and ignore build files

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='carjam',
-    version='1.0.0',
+    version='1.0.1.post1',
     description='Unoffical Python API for CarJam NZ',
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url='https://github.com/kianz20/CarJam-API',
     author='Kian Jazayeri',
     author_email='kianja02@gmail.com',
-    packages=find_packages(),
+    packages=find_packages(exclude=["tests"]),
     zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
